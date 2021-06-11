07769addb7d677a082b873456222ee7812372cfb
Jacoby <40186603+JacobyBlanke@users.noreply.github.com>
2021-06-11 09:59:13 -0400

    Add header

--- HonorRollDeluxe_1/HonorRollDeluxe_1.cpp
+++ HonorRollDeluxe_1/HonorRollDeluxe_1.cpp
@@ -1,3 +1,9 @@
+/**
+  * Names: Jacoby Blanke and Alexei Korolev
+  * Program Name: Honour Roll Deluxe
+  * Date: 06/11/2021
+  * Known Issues: Code is incomplete and doesn't actually do anything useful
+  */ 
 #include <iostream>
 #include "HonorRollClasses.h"
 #include <conio.h>
