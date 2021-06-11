3927c7ffd83d8cdb7aeb0e559e257a526d67e152
Jacoby <40186603+JacobyBlanke@users.noreply.github.com>
2021-06-11 10:08:17 -0400

    Update HonorRollDeluxe_1.cpp

--- HonorRollDeluxe_1/HonorRollDeluxe_1.cpp
+++ HonorRollDeluxe_1/HonorRollDeluxe_1.cpp
@@ -2,7 +2,7 @@
   * Names: Jacoby Blanke and Alexei Korolev
   * Program Name: Honour Roll Deluxe
   * Date: 06/11/2021
-  * Known Issues: Code is incomplete and doesn't actually do anything useful
+  * Known Issues: Code is incomplete. All basic requirements are met, but none of the deluxe requirements are yet fulfilled.
   */ 
 #include <iostream>
 #include "HonorRollClasses.h"
