7f9ec98e621e72f512b83bb19e55d49efb972835
JacobyBlanke <xohvan@gmail.com>
2021-06-11 10:24:31 -0400

    Add new constructor for student object

--- HonorRollDeluxe_1/HonorRollClasses.h
+++ HonorRollDeluxe_1/HonorRollClasses.h
@@ -3,7 +3,7 @@
   * Program Name: Honour Roll Deluxe
   * Date: 06/11/2021
   * Known Issues: Code is incomplete. All basic requirements are met, but none of the deluxe requirements are yet fulfilled.
-  */ 
+  */
 
 #include <string>
 #include <iostream>
@@ -19,76 +19,77 @@ class Course;
 class student
 {
 private:
-  	// Constructable //
-	std::string fName = "", lName = "";
-	bool hasDisciplineIssue = false;
-	std::vector<Course> courses;
-	int average = 0;
+    // Constructable //
+    std::string fName = "", lName = "";
+    bool hasDisciplineIssue = false;
+    std::vector<Course> courses;
+    int average = 0;
 
-	// Non-constructable //
-	bool isEligible = false;
+    // Non-constructable //
+    bool isEligible = false;
 public:
-	student();
-	student(std::string, std::string);
-	void sort_classes();
-	void promptGrades(); // Prompts all the grades of the student, returns true if eligible for HR.
-	void updateAverage(); // Updates the average to match current grades.
-	void updateEligibility(); // Updates the eligibility for Honor Roll
-	void update(); // Updates the entire class
-	int getAvg(); // Returns the current student's average
-	std::string getSaveString(); // Returns the student's data in a string for saving.
+    student();
+    student(std::string, std::string);
+    student(std::string, std::string, double, boolean); //firstname, lastname, total average, disciplinary infraction.
+    void sort_classes();
+    void promptGrades(); // Prompts all the grades of the student, returns true if eligible for HR.
+    void updateAverage(); // Updates the average to match current grades.
+    void updateEligibility(); // Updates the eligibility for Honor Roll
+    void update(); // Updates the entire class
+    int getAvg(); // Returns the current student's average
+    std::string getSaveString(); // Returns the student's data in a string for saving.
   friend std::ostream& operator<<(std::ostream&, const student&);
 };
 
 class Course {
 private:
-	// Constructable //
-	std::string courseName = "";
-	int grade = 0;
+    // Constructable //
+    std::string courseName = "";
+    int grade = 0;
 
 public:
-	// Constructors //
-	Course();
-	Course(std::string);
-	Course(int);
-	Course(std::string, int);
-  
-	// Methods //
-	int getGrade(void); // Returns the current grade of this course;
-	std::string getName(void); // Returns the name of the course;
-	void setGrade(int); // Sets the current grade to a given value
+    // Constructors //
+    Course();
+    Course(std::string);
+    Course(int);
+    Course(std::string, int);
 
-	friend std::ostream& operator<<(std::ostream&, const Course&); // Overloading the '<<' operator for printing.
+    // Methods //
+    int getGrade(void); // Returns the current grade of this course;
+    std::string getName(void); // Returns the name of the course;
+    void setGrade(int); // Sets the current grade to a given value
+
+    friend std::ostream& operator<<(std::ostream&, const Course&); // Overloading the '<<' operator for printing.
 };
 
 class ClassCollection {
-private: 
-	// Contructable //
-	std::string className = "";
-	std::vector<student> students;
-	EasyFile classFile;
-	// ---- File Structure ---- //
-	/*
-		FName;LName;Total_Avg;GPA;Disp_Infraction[Y or N];Class1;Grade;Class2;Grade;Class3;Grade... 
-	*/
+private:
+    // Contructable //
+    std::string className = "";
+    std::vector<student> students;
+    EasyFile classFile;
+    // ---- File Structure ---- //
+    /*
+        FName;LName;Total_Avg;GPA;Disp_Infraction[Y or N];Class1;Grade;Class2;Grade;Class3;Grade...
+    */
 
-	// Unconstructable //
-	bool readSuccessful = false;
-public: 
-	// Constructors //
-	ClassCollection();
-	ClassCollection(std::string);
-	ClassCollection(std::vector<student>);
-	ClassCollection(EasyFile);
-	ClassCollection(std::string, std::vector<student>);
-	ClassCollection(std::vector<student>, EasyFile);
-	ClassCollection(std::string, EasyFile);
-	ClassCollection(std::string, std::vector<student>, EasyFile);
+    // Unconstructable //
+    bool readSuccessful = false;
+public:
+    // Constructors //
+    ClassCollection();
+    ClassCollection(std::string);
+    ClassCollection(std::vector<student>);
+    ClassCollection(EasyFile);
+    ClassCollection(std::string, std::vector<student>);
+    ClassCollection(std::vector<student>, EasyFile);
+    ClassCollection(std::string, EasyFile);
+    ClassCollection(std::string, std::vector<student>, EasyFile);
 
-	// Methods //
-	void setPath(std::string);
-	void setFile(EasyFile);
-	void addStudent(student);
-	void removeStudent(student);
-	bool readData();
+    // Methods //
+    void setPath(std::string);
+    void setFile(EasyFile);
+    void addStudent(student);
+    void removeStudent(student);
+    bool readData();
 };
